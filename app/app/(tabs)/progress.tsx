c8335638725cd1be774a1d9682a0ce0c37cc8b95
realhardik18 <realhardik18@gmail.com>
2025-09-22 01:14:49 +0530

    swapped out icons to use lucide-react-native icons instead of sf symbols

--- app/app/(tabs)/progress.tsx
+++ app/app/(tabs)/progress.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function HomeScreen() {
+export default function ProgressScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Progress</Text>
     </View>
   );
 }
